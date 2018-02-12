44b7776bee37c380a26db1d6e76c480827996e48
Corentin Chary <c.chary@criteo.com>
2018-02-12 09:50:48 +0100

    1.2.0-criteo7

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def read(fname):
 try:
     setup(
       name='graphite-web',
-      version='1.2.0-criteo6',
+      version='1.2.0-criteo7',
       url='http://graphiteapp.org/',
       author='Chris Davis',
       author_email='chrismd@gmail.com',
