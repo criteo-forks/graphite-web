7ce5ef3b49c2c766086cca33897c4912464b2e89
Jean-Francois Weber-Marx <jf.webermarx@criteo.com>
2019-02-15 08:25:02 +0000

    1.2.0-criteo9
    Signed-off-by: Jean-Francois Weber-Marx <jf.webermarx@criteo.com>

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ examples = [ ('examples', glob('examples/example-*')) ]
 try:
     setup(
       name='graphite-web',
-      version='1.2.0-criteo8',
+      version='1.2.0-criteo9',
       url='http://graphiteapp.org/',
       author='Chris Davis',
       author_email='chrismd@gmail.com',
